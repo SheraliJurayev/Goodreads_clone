fb1fd304c5b5a1ec29afc4c084c1bbdb1a2a715e
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-03-25 14:26:49 +0500

    Change something

--- goodreads/settings.py
+++ goodreads/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'goodreads.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'goodreads',
+        'HOST': 'localhost',
+        'PORT': '5432' , 
+        'USER': 'sh_jurayeff' , 
+        'PASSWORD': 'Sony1122'
     }
 }
 
