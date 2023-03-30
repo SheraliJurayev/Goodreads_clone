1e16c10c191391d305b276ff469a65cb954fdc78
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-03-30 23:13:28 +0500

    .....

--- users/forms.py
+++ users/forms.py
@@ -2,27 +2,14 @@ from django import forms
 from django.contrib.auth.models import User
 
 class UserCreateForm(forms.ModelForm):
-    username = forms.CharField(max_length=200)
-    first_name = forms.CharField(max_length=200)
-    last_name = forms.CharField(max_length=200)
-    email = forms.EmailField()
-    password = forms.CharField(max_length=128)
+    class Meta:
+        model = User
+        fields = ('username','first_name','last_name','email','password')
 
-    def save(self):
-            username = self.cleaned_data['username']
-            first_name = self.cleaned_data['first_name']
-            last_name = self.cleaned_data['last_name']    
-            email = self.cleaned_data['email'] 
-            password = self.cleaned_data['password']
 
-            user = User.objects.create(
-                username=username ,
-                first_name=first_name ,
-                last_name=last_name , 
-                email=email,
-                password=password
-            )
-            user.set_password(password)
-            user.save() 
+    def save(self , commit=True):   # xavfsizlik uchun passwordni hash qilish
+        user = super().save(commit)
+        user.set_password(self.cleaned_data['password'])
+        user.save()
 
-            return user
+        return user     
--- users/views.py
+++ users/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render , redirect
 from django.views import View
 from django.contrib.auth.models import User
 from users.forms import UserCreateForm
-# Create your views here.
 
-print()
 class RegisterView(View):
     def get(self, request):
 
@@ -19,16 +17,18 @@ class RegisterView(View):
 
         if create_form.is_valid():
             create_form.save()
-            return ('users:login_page')
+            return redirect('users:login_page')
         else:
             context = {
             'form': create_form
             }
             return render(request, 'users/register.html' , context)
-    
-    
+
 
 class LoginView(View):
     def get(self, request):
         return render(request, 'users/login.html')    
+    
+
+
     
