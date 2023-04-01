1f423b86b1e45f7926736dd469465445fc8a3c85
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-04-01 21:29:42 +0500

    Abobut learning Middleware

--- goodreads/middleware.py
+++ goodreads/middleware.py
@@ -1,15 +1,12 @@
+## Abobut learning Middleware
+
 class SimpleMiddleware:
+
     def __init__(self, get_response):
         self.get_response = get_response
-        # One-time configuration and initialization.
 
     def __call__(self, request):
-        # Code to be executed for each request before
-        # the view (and later middleware) are called.
-
+        print(f'Before request for {request.path}')
         response = self.get_response(request)
-
-        # Code to be executed for each request/response after
-        # the view is called.
-
+        print('After getting request  ')
         return response
